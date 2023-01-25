9e071317788410d3bde35754fbf34ceeb692c536
macbookair2020 <uskanovjahongir1199@gmail.com>
2023-01-25 21:25:31 +0500

    dfdfdfdf

--- lib/constants/url_constants.dart
+++ lib/constants/url_constants.dart
@@ -1,4 +1,5 @@
 class BASE_URL {
-  static String SIGN_UP_API = "http://api.imtest.uz/v1/auth/register/";
-  static String SIGN_IN_API = "http://api.imtest.uz/v1/auth/login/";
+  static String HOST =  "http://online-test.rmysmo.uz/";
+  static String SIGN_UP_API = HOST + "v1/auth/register/";
+  static String SIGN_IN_API = HOST + "v1/auth/login/";
 }
