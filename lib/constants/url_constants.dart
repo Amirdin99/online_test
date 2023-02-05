666f410556334701879d93ff45cbe7036952e581
Amirdin99 <amirdinamirdin1999@gmail.com>
2023-02-05 19:33:36 +0500

    Merge remote-tracking branch 'origin/master'

--- lib/constants/url_constants.dart
+++ lib/constants/url_constants.dart
@@ -2,7 +2,7 @@ class BASE_URL {
   static String BASE_URl="http://online-test.rmysmo.uz";
 
   static String SIGN_UP_API = BASE_URl+"/v1/auth/register/";
-  static String SIGN_IN_API =BASE_URl+"/v1/auth/login/";
+  static String LOGIN =BASE_URl+"/v1/auth/login/";
   static String POST_APLICATION=BASE_URl+"/v1/center/application";
   static String PUT_APLICATION=BASE_URl+"/v1/center/application/20";
   static String GET_APLICATION_LIST=BASE_URl+"/v1/center/applications";
